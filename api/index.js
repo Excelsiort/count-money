9b0860c5f06cb4ccf96e5e5553e7c21c4eb02382
excelsiort <raphael.zeitoun@epitech.eu>
2022-11-15 11:29:00 +0100

    connection db

---
+++ api/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+require('dotenv').config({path: './config/.env'});
+require('./config/db'); 
+const app = express();
+
+app.listen(process.env.PORT, () => console.log('serveur started :' + process.env.PORT));
