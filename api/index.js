797324d651a7bad3d7d3b3bffe48f874fd3b06bb
ratdoux <radu.gheorghiu@epitech.eu>
2022-11-15 13:55:38 +0100

    dsdqs

--- api/index.js
+++ api/index.js
@@ -3,7 +3,8 @@ require('dotenv').config({path: './config/.env'});
 require('./config/db');
 const userRoutes = require("./routes/user.routes");
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 //routes
 app.use('/api/user', userRoutes);
 
