a66038575b0f085ea3aacb14c9b4f3766670f029
excelsiort <raphael.zeitoun@epitech.eu>
2022-11-15 16:52:07 +0100

    jwt

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const { requireAuth, checkUser } = require('./middleware/auth.middleware')
 const cookieParser = require('cookie-parser');
 require('dotenv').config({path: './config/.env'});
 require('./config/db');
@@ -10,6 +11,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
+
+
+//jwt
+app.get('*', checkUser);
+app.get('/jwtid', requireAuth, (req, res) => {
+    res.status(200).send(res.locals.user._id)
+})
 
 //routes
 app.use('/api/user', userRoutes);
